e2a6d67a7cebf6dc882d634c64713285ec861a89
hawkjosh <hawkjosh@icloud.com>
2024-03-13 11:00:01 -0400

    separated out exploration from tutorials and did some other directory restructuring to clean it up

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TimeGPTExplore',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # list project dependencies here
+    ],
+)
